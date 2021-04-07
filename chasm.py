15b1ba89919a53ef09751b2794c9cbcc5b916756
JonLiuFYI <2997583+JonLiuFYI@users.noreply.github.com>
2021-04-07 00:27:39 -0400

    Chasm complete

--- chasm.py
+++ chasm.py
@@ -16,33 +16,63 @@ class Chasm:
 
         self.gradient = [6, 12, 5, 1, 0, 0, 1, 5, 12, 6]
 
-        self.gradient_w = 3
+        self.gradient_w = 2
         self.gradient_offset = (
             WIDTH // 2 - self.gradient_w * (len(self.gradient) // 2) + 1
         )
 
-        self.strip_w = 45
+        self.strip_w = 40
         self.strip_h = HEIGHT // 4
         self.strips = [
             # left
+            Strip(0, HEIGHT + 5 * self.strip_h, self.strip_w, self.strip_h),
+            Strip(0, HEIGHT + 3 * self.strip_h, self.strip_w, self.strip_h),
+            Strip(0, HEIGHT + self.strip_h, self.strip_w, self.strip_h),
             Strip(0, HEIGHT - self.strip_h, self.strip_w, self.strip_h),
             Strip(0, HEIGHT - 3 * self.strip_h, self.strip_w, self.strip_h),
             Strip(0, HEIGHT - 5 * self.strip_h, self.strip_w, self.strip_h),
             # right
             Strip(
-                WIDTH - self.strip_w, HEIGHT - self.strip_h, self.strip_w, self.strip_h
+                WIDTH - self.strip_w,
+                HEIGHT + 5 * self.strip_h,
+                self.strip_w,
+                self.strip_h,
+                tri_on_right=False,
+            ),
+            Strip(
+                WIDTH - self.strip_w,
+                HEIGHT + 3 * self.strip_h,
+                self.strip_w,
+                self.strip_h,
+                tri_on_right=False,
+            ),
+            Strip(
+                WIDTH - self.strip_w,
+                HEIGHT + self.strip_h,
+                self.strip_w,
+                self.strip_h,
+                tri_on_right=False,
+            ),
+            Strip(
+                WIDTH - self.strip_w,
+                HEIGHT - self.strip_h,
+                self.strip_w,
+                self.strip_h,
+                tri_on_right=False,
             ),
             Strip(
                 WIDTH - self.strip_w,
                 HEIGHT - 3 * self.strip_h,
                 self.strip_w,
                 self.strip_h,
+                tri_on_right=False,
             ),
             Strip(
                 WIDTH - self.strip_w,
                 HEIGHT - 5 * self.strip_h,
                 self.strip_w,
                 self.strip_h,
+                tri_on_right=False,
             ),
         ]
 
@@ -55,6 +85,8 @@ class Chasm:
         fc = px.frame_count
         px.cls(7)
 
+        px.rect(self.strip_w + 1, 0, WIDTH - 2 * self.strip_w, HEIGHT, 6)
+
         for i, c in enumerate(self.gradient):
             px.rect(
                 i * self.gradient_w + self.gradient_offset,
@@ -66,24 +98,32 @@ class Chasm:
 
         [s.draw() for s in self.strips]
 
-        # FIXME: centre for reference
-        px.pset(WIDTH // 2, HEIGHT // 2, 8)
-
 
 class Strip:
-    def __init__(self, x, y, w, h):
+    def __init__(self, x, y, w, h, *, tri_on_right=True):
         self.x = x
         self.y = y
         self.w = w
         self.h = h
+        self.tri_on_right = tri_on_right
 
     def update(self):
         self.y -= 1
-        if self.y < -2 * self.h:
-            self.y = HEIGHT
+        if self.y < -4 * self.h:
+            self.y = HEIGHT + 4 * self.h
 
     def draw(self):
         px.rect(self.x, self.y, self.w, self.h, 0)
+
+        px.tri(
+            self.x + (self.w if self.tri_on_right else 0),
+            self.y,
+            self.x + (self.w if self.tri_on_right else 0),
+            self.y + self.h - 1,  # -1 for aesthetics
+            WIDTH // 2,
+            HEIGHT // 2,
+            0,
+        )
 
 
 Chasm()
