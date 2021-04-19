62ad318c2cf1a0c5fe8b3cf06e38e09857e5471f
JonLiuFYI <2997583+JonLiuFYI@users.noreply.github.com>
2021-04-19 01:07:20 -0400

    sprite_orbit: wavy logo

--- sprite_orbit.py
+++ sprite_orbit.py
@@ -18,17 +18,19 @@ class SpriteOrbit:
         px.load("assets/demo-fx.pyxres")
 
         self.sprites = [Sprite(x * 0.25 * pi, over=(not 2 <= x <= 5)) for x in range(8)]
+        self.logostrips = [LogoStrip(n) for n in range(0, 38)]
 
         px.run(self.update, self.draw)
 
     def update(self):
         [s.update() for s in self.sprites]
+        [strip.update() for strip in self.logostrips]
 
     def draw(self):
         px.cls(0)
 
         [s.draw() for s in self.sprites if not s.over]
-        px.blt(26, 48, 0, 0, 0, 76, 32, 0)  # TODO: wave logo up and down
+        [strip.draw() for strip in self.logostrips]
         [s.draw() for s in self.sprites if s.over]
 
 
@@ -61,6 +63,22 @@ class Sprite:
 
     def draw(self):
         px.blt(self.x - 3, self.y - 3, 0, 8 * self.over, 32, 8, 8, 14)
+
+
+class LogoStrip:
+    def __init__(self, stripnum):
+        self.stripnum = stripnum
+        self.x = 26 + 2 * stripnum
+        self.y = 48
+
+    def update(self):
+        self.y = 6 * sin(px.frame_count / 20 + self.stripnum / 12) + 48
+
+    def draw(self):
+        px.blt(self.x, self.y, 0,
+                2 * self.stripnum, 0,
+                2, 32,
+                0)
 
 
 SpriteOrbit()
